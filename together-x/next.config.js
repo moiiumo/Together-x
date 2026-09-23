9fa5af8355568dd03ab52ca855cb71fce04f3714
moiiumo <106795221+moiiumo@users.noreply.github.com>
2026-09-23 17:46:27 +0700

    Delete together-x directory

--- together-x/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: { unoptimized: true }, // Cloudflare Pages doesn't support next/image optimization API
-};
-
-module.exports = nextConfig;
